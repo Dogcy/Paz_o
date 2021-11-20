9c8de049afb2445c77aa95537745531ca96ebe8e
DOGCY <ccy810127@gmail.com>
2021-11-20 18:42:37 +0800

    新增 controller PazDemo View 修改layout連結

---
+++ Paz_o/Controllers/PazDemo.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Paz_o.Controllers
+{
+    public class PazDemo : Controller
+    {
+
+        private readonly ILogger<PazDemo> _logger;
+
+        public PazDemo(ILogger<PazDemo> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
