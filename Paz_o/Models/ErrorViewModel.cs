8d18745ab08a27aae79f3e0181d2c4ba7b0f10ae
DOGCY <ccy810127@gmail.com>
2021-11-20 18:04:38 +0800

    firstCommit

---
+++ Paz_o/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Paz_o.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
